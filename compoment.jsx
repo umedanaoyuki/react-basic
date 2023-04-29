dc31bc163ccecdeafd68ca7f097b322c0e064f0f
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-04-29 16:53:41 +0900

    reactDOM.renader 追加

--- compoment.jsx
+++ compoment.jsx
@@ -1,5 +1,23 @@
 //関数コンポーネント
-
 const App = () => {
     return <p>これは関数コンポーネントです</p>
 }
+
+// ReactDOM.render
+
+//HTML
+<div id="root"></div>
+
+//このHTMLのdiv要素にReact要素をレンダリングしたい場合は以下のコードを記載
+
+import { ReactDOM } from "react-dom";
+
+//helloコンポーネント（関数型コンポーネント）
+const Hello = () => {
+    return <h1>Hello, React</h1>
+}
+
+//ReactDOM.render()で<div id="root"></div>に<Hello />を割り当てる
+ReactDOM.render(<Hello />, document.getElementById("root"));
+
+
